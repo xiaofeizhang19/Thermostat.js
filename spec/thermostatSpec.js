dd2a2d8c628232e1a11a0af0a45500bda40e43eb
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MacBook-Pro.local>
2019-09-18 18:04:29 +0100

    Refactor functions

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -4,6 +4,7 @@ describe('Thermostat', function() {
     beforeEach(function() {
       thermostat = new Thermostat();
     });
+    
     it('has default temperature of 20', function() {
       expect(thermostat.temperature).toEqual(20)
     });
@@ -16,18 +17,18 @@ describe('Thermostat', function() {
       expect(thermostat.temperature).toEqual(19)
     });
     it('minimum temparature set to 10', function() {
-      for (var i =1; i <= 10; i++) thermostat.down();
+      for (var i = 1; i <= 10; i++) thermostat.down();
       expect(function() { thermostat.down() }).toThrow('Minimum temperature set to 10')
     });
 
     it('power saving mode set to on by default', function() {
-      expect(thermostat.powerSaving).toEqual(true)
+      expect(thermostat.powerSaving).toEqual('on')
     });
     it('switch power saving mode', function() {
       thermostat.powerSavingSwitch();
-      expect(thermostat.powerSaving).toEqual(false)
+      expect(thermostat.powerSaving).toEqual('off')
       thermostat.powerSavingSwitch();
-      expect(thermostat.powerSaving).toEqual(true)
+      expect(thermostat.powerSaving).toEqual('on')
     });
     it('maximum temperature is 25 degrees with power saving on', function() {
       for (var i = 1; i <= 5; i++)
@@ -66,83 +67,3 @@ describe('Thermostat', function() {
     });
   });
 });
-
-
-
-// describe('Airport', function() {
-//   var airport;
-//   describe('#landing', function() {
-//     airport = new Airport();
-//     // beforeEach(function() {
-//     // airport = {
-//     //   land: function(value) {return value;}
-//     // };
-//     it('can be instructed to land a plane', function() {
-//       spyOn(airport, 'land');
-//       plane = jasmine.createSpy;
-//       airport.land(plane)
-//       expect(airport.land).toHaveBeenCalled();
-//     });
-//     it('when instructed to land plane, airport containts plane', function() {
-//       airport = new Airport();
-//       spyOn(airport, "getWeather").and.returnValue('sunny');
-//       plane = jasmine.createSpy;
-//       airport.land(plane);
-//       expect(airport.getPlanes()).toEqual([plane])
-//     });
-//     it('no landing when weather is stormy', function() {
-//       airport = new Airport();
-//       spyOn(airport, "getWeather").and.returnValue('stormy');
-//       plane = jasmine.createSpy;
-//       expect(function() {airport.land(plane)}).toThrow("Stormy - no plane movements at airport")
-//       expect(airport.getPlanes()).toEqual([])
-//     });
-//     it('no landing when capacity is reached', function() {
-//       airport = new Airport(1);
-//       spyOn(airport, "getWeather").and.returnValue('sunny');
-//       plane = jasmine.createSpy;
-//       airport.land(plane)
-//       expect (function() {airport.land(plane)}).toThrow("Capacity exceeded")
-//     });
-//
-//   });
-// })
-//
-// describe('Airport', function() {
-//   var airport;
-//   describe('#take/-off', function() {
-//     airport = new Airport();
-//
-//     it('can be instructed to have plane take off ', function() {
-//       spyOn(airport, "getWeather").and.returnValue('sunny');
-//       plane = jasmine.createSpy;
-//       spyOn(airport, 'takeoff');
-//       airport.land(plane);
-//       airport.takeoff(plane);
-//       expect(airport.takeoff).toHaveBeenCalled();
-//     });
-//     it('plane instructed to take off is not in array anymore', function() {
-//       airport = new Airport();
-//       spyOn(airport, "getWeather").and.returnValue('sunny');
-//       plane = jasmine.createSpy;
-//       plane2 = jasmine.createSpy;
-//       airport.land(plane)
-//       airport.land(plane2)
-//       airport.takeoff(plane)
-//       expect(airport.getPlanes()).toEqual([plane2]);
-//     });
-//     it('plane cannot take off when weather is stormy', function() {
-//       airport = new Airport();
-//       plane = jasmine.createSpy;
-//       airport.hangar.push(plane);
-//       spyOn(airport, "getWeather").and.returnValue('stormy');
-//       expect(function() { airport.takeoff(plane) }).toThrow("Stormy - no plane movements at airport")
-//     });
-//     it('plane cannot take off when not at airport', function() {
-//       airport = new Airport();
-//       plane = jasmine.createSpy;
-//       spyOn(airport, "getWeather").and.returnValue('sunny');
-//       expect(function() { airport.takeoff(plane) }).toThrow("Plane not at airport")
-//     });
-//   });
-// })
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,33 +1,35 @@
 function Thermostat() {
   this.temperature = 20
-  this.powerSaving = true
+  this.powerSaving = 'on'
   this.energyUsage = 'medium-usage'
 };
 
 Thermostat.prototype.up = function() {
-  if (this.powerSaving) {
-    if (this.temperature < 25) {
-      this.temperature += 1;
-    } else {
+  if (this.powerSaving === 'on') {
+    if (this.temperature + 1 > 25) {
       throw 'Maximum temperature set to 25'
     }
   } else {
-    if (this.temperature < 32) {
-      this.temperature += 1;
-    } else {
+    if (this.temperature + 1 > 32) {
       throw 'Maximum temperature set to 32'
-    }
+    } 
   }
+  this.temperature += 1;
 };
 
 Thermostat.prototype.down = function() {
-  if (this.temperature > 10) {
-  this.temperature -= 1;}
-  else { throw 'Minimum temperature set to 10'}
+  if (this.temperature -1 < 10) {
+    throw 'Minimum temperature set to 10' 
+  } 
+  this.temperature -= 1;
 };
 
 Thermostat.prototype.powerSavingSwitch = function() {
-  this.powerSaving = !this.powerSaving
+  if (this.powerSaving === 'on') {
+    this.powerSaving = 'off';
+  } else {
+    this.powerSaving = 'on';
+  }
 };
 
 Thermostat.prototype.reset = function() {
@@ -43,50 +45,3 @@ Thermostat.prototype.updateUsage = function() {
     this.energyUsage = 'medium-usage';
   }
 };
-
-
-// Thermostat.prototype.getTemperature = function() {
-//   return this.temperature;
-// };
-
-
-// function Airport(capacity = 20) {
-//    this.hangar = []
-//    this.capacity = capacity
-// };
-//
-// Airport.prototype.land  = function(plane) {
-//   if (this.getWeather() === 'stormy') {
-//     throw "Stormy - no plane movements at airport"
-//   };
-//   if (this.capacity === this.hangar.length) {
-//     throw "Capacity exceeded"
-//   };
-//   this.hangar.push(plane);
-// };
-//
-// Airport.prototype.getPlanes = function() {
-//   return this.hangar
-// };
-//
-// Airport.prototype.getWeather = function() {
-//   random_number = Math.floor(Math.random() * 10)
-//   if (random_number === 0) {
-//     return 'stormy'
-//   } else {
-//     return 'sunny'
-//   }
-// };
-//
-// Airport.prototype.takeoff = function(plane){
-//   if (this.getWeather() === 'stormy') {
-//     throw "Stormy - no plane movements at airport"
-//   };
-//   if (this.hangar.includes(plane) === false) {
-//     throw "Plane not at airport"
-//   };
-//    var index = this.hangar.indexOf(plane)
-//    if (index > -1){
-//        this.hangar.splice(index, 1)
-//     };
-// };
